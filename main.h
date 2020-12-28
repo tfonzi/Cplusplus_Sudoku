246c68c8fed9ace47e85cf7fb75931d2fd41a330
tfonzi <tyler.a.fonzi@gmail.com>
2020-12-28 17:21:40 -0500

    Seperated Sudoku_array data vector and start function from QT side. Implemented from inside of main.cpp

--- main.h
+++ main.h
@@ -1,4 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string>
+#include <vector>
+#include "cell.h"
+
+using namespace std;
+
+void start(string sudoku_string, vector<cell> &sudoku_array);
+//Populates cell vector "sudoku_array" using input string
+
+
+
+
+
+
 #endif // MAIN_H
--- mainwindow.h
+++ mainwindow.h
@@ -3,9 +3,7 @@
 
 #include <QMainWindow>
 #include "cell.h"
-#include <string>
 
-using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -18,12 +16,6 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
-    //global vector
-    vector<cell> sudoku_array;
-
-    void start(string sudoku_string);
-    //Populates cell vector "sudoku_array" using input string
 
     void update(cell* sudoku_array);
     //Updates ui grid to match sudoku array
